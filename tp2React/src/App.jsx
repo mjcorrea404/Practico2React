3b51e31ef515fccd81e4f9933020077782d694d0
Julieta Correa <mjcorrea210@gmail.com>
2023-05-31 20:37:14 -0300

    primer commit

---
+++ tp2React/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello my friend!</h1>
+    </div>
+  );
+}
+
+export default App;
+
